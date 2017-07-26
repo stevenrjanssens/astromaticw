caf8e674f5a783f47854ba6dce77cb3dd275dbca
Steven Janssens <janssens@astro.utoronto.ca>
2017-07-26 16:53:49 -0400

    Fixed url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	version='0.1.0',
 	description='A low-level Source Extractor wrapper',
 	author='Steven Janssens',
-	url='http://github.com/stevenrjanssens/SExtractorW',
+	url='http://github.com/stevenrjanssens/sextractorw',
 	packages=['sextractorw'],
 	package_dir={'sextractorw': 'src/sextractorw'},
         package_data={'sextractorw': ['config/*']}

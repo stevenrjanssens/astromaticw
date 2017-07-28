c640d99cfe809ec59ddef44a7646a8999f56748c
Steven Janssens <janssens@astro.utoronto.ca>
2017-07-27 20:49:42 -0400

    Python 3 needs absolute imports

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='SExtractorW',
-	version='0.1.1',
+	version='0.1.2',
 	description='A low-level Source Extractor wrapper',
 	author='Steven Janssens',
 	url='http://github.com/stevenrjanssens/sextractorw',

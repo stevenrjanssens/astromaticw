b381ba4f75bba40b2d3f961364aaf64fb7645131
Steven Janssens <janssens@astro.utoronto.ca>
2017-07-27 21:37:13 -0400

    Renamed module

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# SExtractorW
+# astromaticW
 
 The test region is [1525:2476,3700:4701] from the A2744-HFFPAR F814W image
--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='SExtractorW',
-	version='0.1.2',
-	description='A low-level Source Extractor wrapper',
+	name='astromaticW',
+	version='0.2.0',
+	description='Low-level astromatic wrappers',
 	author='Steven Janssens',
-	url='http://github.com/stevenrjanssens/sextractorw',
+	url='http://github.com/stevenrjanssens/astromaticw',
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
-        package_data={'sextractorw': ['config/*']}
+        package_data={'astromaticw': ['config/*']}
 )
